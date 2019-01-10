2625c660fe40bd84dcca22fdb7679070f3a47f2c
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2019-01-10 13:58:35 -0600

    Add files via upload

--- shapeObj.js
+++ shapeObj.js
@@ -1,17 +1,85 @@
 //Your code
 
 let Rectangle = {
+   
+   
 
+   
+    area: function(){
+        
+        let a = ;
+        
+        return a;
+    },
+    perimeter: function(){
+        
+        let p = 2 * this.length + 2 * this.width;
+
+        return p;
+    }
 }
 
 let Square = {
+    sideLength: 4,
 
+    perimeter: function(){
+        
+        let p = this.sideLength * 4;
+        
+        return p
+    },
+    area: function(){
+        
+        let a = this.sidelength * this.sidelength;
+        
+        return a;
+    }
 }
 
 let Circle = {
+    raduis: 3,
+    circumference: function(){
+       
+        let c = 2* this.raduis * 3.14;
+
+        return c; 
+    },
+     area: function(){
+      
+        let a = Math.PI * this.raduis * this.raduis;
+
+        return a; 
+    }
+}
+
+let Box = {
+    
+    length: 6,
+    width: 6,
+    height: 10,
+
+    volume: function(){
+
+        let v = this.length *  this.length *  this.length;
+        
+        return v
+
+    }
 
 }
 
+let Sphere = {
+    
+    radius: 8,
+    
+     volume: function(){
+
+        let v = 4/3 * Math.PI * this.raduis *  this.radius *  this.radius;
+        
+        return v;
+    }
+
+}
 //Dont forget about box and sphere
 
 
